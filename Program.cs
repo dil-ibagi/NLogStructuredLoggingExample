ebdde56d0cd8203ed3a6d2a81021efff77daaa23
dil-ibagi <ibagi@diligent.com>
2023-08-15 13:52:33 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NLogStructuredLogging
+{
+    internal class Program
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] _)
+        {
+            logger.Info("This is a structured log with primitives. Properties: {Text} {Number}", 
+                "MyText", 49);
+
+            logger.Info("This is a structured log with a complex object. Properties: {}", 
+                new { Id = 1, Timestamp = DateTime.UtcNow, Tag = "tag1" });
+
+            try
+            {
+                throw new InvalidOperationException("This is a structured log with an exception");
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.Error(ex);
+            }
+
+            var logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", $"{DateTime.Today:yyyy-MM-dd}.log");
+            Process.Start(logPath);
+        }
+    }
+}
